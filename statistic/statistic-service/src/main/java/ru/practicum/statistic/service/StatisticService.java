1e16b567804ffa2875a718bd8ac6762776db84b8
MaricTheirin <ghostmeee@yandex.ru>
2023-04-18 20:56:42 +0300

    feat: Добавить реализацию StatisticService

--- statistic/statistic-service/src/main/java/ru/practicum/statistic/service/StatisticService.java
+++ statistic/statistic-service/src/main/java/ru/practicum/statistic/service/StatisticService.java
@@ -1,12 +1,13 @@
 package ru.practicum.statistic.service;
 
 import ru.practicum.statistic.dto.EndpointHitDto;
-import ru.practicum.statistic.dto.ViewStatsDto;
+import ru.practicum.statistic.dto.EndpointHitsResultDto;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface StatisticService {
 
-    ViewStatsDto getStats(LocalDateTime start, LocalDateTime end, String[] uris, Boolean unique);
+    List<EndpointHitsResultDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 
     void saveHit(EndpointHitDto hitDto);
 
--- statistic/statistic-service/src/main/java/ru/practicum/statistic/service/StatisticServiceImpl.java
+++ statistic/statistic-service/src/main/java/ru/practicum/statistic/service/StatisticServiceImpl.java
@@ -1,19 +1,45 @@
 package ru.practicum.statistic.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.statistic.dto.EndpointHitDto;
-import ru.practicum.statistic.dto.ViewStatsDto;
+import ru.practicum.statistic.dto.EndpointHitsResultDto;
+import ru.practicum.statistic.mapper.EndpointHitDtoMapper;
+import ru.practicum.statistic.model.EndpointHit;
+import ru.practicum.statistic.model.EndpointStats;
+import ru.practicum.statistic.repository.EndpointHitRepository;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class StatisticServiceImpl implements StatisticService {
 
+    private final EndpointHitRepository endpointHitRepository;
+
     @Override
-    public ViewStatsDto getStats(LocalDateTime start, LocalDateTime end, String[] uris, Boolean unique) {
-        return null;
+    public List<EndpointHitsResultDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        List<EndpointStats> stats;
+        if (uris == null || uris.isEmpty()) {
+            stats = endpointHitRepository.findAllByTime(start, end);
+        } else if (!unique) {
+            stats = endpointHitRepository.findAllByTimeAndFilterByUri(start, end, uris);
+        } else {
+            stats = endpointHitRepository.findAllByTimeAndFilterByUriInAndUniqueIp(start, end, uris);
+        }
+        log.info("Получен список обращений: {}", stats);
+        return stats.stream().map(EndpointHitDtoMapper::mapEndpointStatsToDto).collect(Collectors.toList());
     }
 
+    @Transactional
     @Override
     public void saveHit(EndpointHitDto hitDto) {
-
+        EndpointHit endpointHit = endpointHitRepository.saveAndFlush(EndpointHitDtoMapper.mapDtoToEndpointHit(hitDto));
+        log.info("Обращение {} сохранено", endpointHit);
     }
 
 }
