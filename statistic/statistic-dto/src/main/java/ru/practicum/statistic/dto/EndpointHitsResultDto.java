e1aa3ae827749bf4493aa402c8fd900272921c76
MaricTheirin <ghostmeee@yandex.ru>
2023-04-19 19:01:34 +0300

    chore: Убрать лишнюю валидацию полей, добавить Value

--- statistic/statistic-dto/src/main/java/ru/practicum/statistic/dto/EndpointHitsResultDto.java
+++ statistic/statistic-dto/src/main/java/ru/practicum/statistic/dto/EndpointHitsResultDto.java
@@ -1,18 +1,12 @@
 package ru.practicum.statistic.dto;
 
 import lombok.*;
-import javax.validation.constraints.NotBlank;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+@Value
 public class EndpointHitsResultDto {
 
-    @NotBlank
     String app;
 
-    @NotBlank
     String uri;
 
     long hits;
