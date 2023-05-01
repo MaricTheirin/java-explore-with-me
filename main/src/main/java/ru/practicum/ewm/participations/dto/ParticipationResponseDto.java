18123383e326a0fb4a6f12122886c66339698fe5
MaricTheirin <ghostmeee@yandex.ru>
2023-05-01 13:16:05 +0300

    chore: Добавить model/dto под Participation

---
+++ main/src/main/java/ru/practicum/ewm/participations/dto/ParticipationResponseDto.java
@@ -0,0 +1,10 @@
+package ru.practicum.ewm.participations.dto;
+
+import ru.practicum.ewm.participations.model.ParticipationState;
+
+public class ParticipationResponseDto {
+
+    int id;
+    ParticipationState status;
+
+}
---
+++ main/src/main/java/ru/practicum/ewm/participations/model/ParticipationState.java
@@ -0,0 +1,9 @@
+package ru.practicum.ewm.participations.model;
+
+public enum ParticipationState {
+
+    PENDING,
+    CONFIRMED,
+    CANCELLED
+
+}
