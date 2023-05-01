8fe438abf97a4df13b0eca735262098845de1b06
MaricTheirin <ghostmeee@yandex.ru>
2023-05-01 13:11:56 +0300

    feat: Добавить маппинг UserShortResponseDto

--- main/src/main/java/ru/practicum/ewm/users/dto/UserResponseDto.java
+++ main/src/main/java/ru/practicum/ewm/users/dto/UserResponseDto.java
@@ -2,9 +2,11 @@ package ru.practicum.ewm.users.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public class UserResponseDto {
 
     private long id;
--- main/src/main/java/ru/practicum/ewm/users/mapper/UserDtoMapper.java
+++ main/src/main/java/ru/practicum/ewm/users/mapper/UserDtoMapper.java
@@ -6,13 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import ru.practicum.ewm.service.mapper.Mapper;
 import ru.practicum.ewm.users.dto.UserDto;
 import ru.practicum.ewm.users.dto.UserResponseDto;
+import ru.practicum.ewm.users.dto.UserShortResponseDto;
 import ru.practicum.ewm.users.model.User;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Slf4j
 public class UserDtoMapper extends Mapper {
 
-    public static User mapDtoToUser (UserDto userDto) {
+    public static User mapDtoToUser(UserDto userDto) {
         User user = new User(
                 0L,
                 userDto.getName(),
@@ -30,6 +31,15 @@ public class UserDtoMapper extends Mapper {
         );
         log.trace(DEFAULT_MESSAGE, user, userResponseDto);
         return userResponseDto;
+    }
+
+    public static UserShortResponseDto mapUserToShortResponseDto(User user) {
+        UserShortResponseDto userShortResponseDto = new UserShortResponseDto(
+                user.getId(),
+                user.getName()
+        );
+        log.trace(DEFAULT_MESSAGE, user, userShortResponseDto);
+        return userShortResponseDto;
     }
 
 }
