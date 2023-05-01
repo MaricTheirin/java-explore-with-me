ddd438906600ff3e3d26b2fce7b151de568ac3a7
MaricTheirin <ghostmeee@yandex.ru>
2023-05-01 13:08:43 +0300

    chore: Мелкие правки Category

--- main/src/main/java/ru/practicum/ewm/categories/dto/CategoryDto.java
+++ main/src/main/java/ru/practicum/ewm/categories/dto/CategoryDto.java
@@ -1,8 +1,12 @@
 package ru.practicum.ewm.categories.dto;
 
 import lombok.Data;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class CategoryDto {
+
+    @NotBlank
     private String name;
+
 }
--- main/src/main/java/ru/practicum/ewm/categories/exception/CategoryNotFoundException.java
+++ main/src/main/java/ru/practicum/ewm/categories/exception/CategoryNotFoundException.java
@@ -3,9 +3,7 @@ package ru.practicum.ewm.categories.exception;
 import ru.practicum.ewm.service.exception.NotFoundException;
 
 public class CategoryNotFoundException extends NotFoundException {
-
     public CategoryNotFoundException(long id) {
         super("Категория с id = " + id + " не обнаружена");
     }
-
 }
