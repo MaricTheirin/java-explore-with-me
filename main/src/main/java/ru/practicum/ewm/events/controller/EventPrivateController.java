d747802c73842a308ede702ea7a7c84608227a19
MaricTheirin <ghostmeee@yandex.ru>
2023-05-03 16:12:58 +0300

    feat: Добавить валидацию в контроллере

--- main/src/main/java/ru/practicum/ewm/events/controller/EventPrivateController.java
+++ main/src/main/java/ru/practicum/ewm/events/controller/EventPrivateController.java
@@ -3,17 +3,22 @@ package ru.practicum.ewm.events.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.events.dto.EventDto;
 import ru.practicum.ewm.events.dto.EventResponseDto;
 import ru.practicum.ewm.events.dto.EventShortResponseDto;
 import ru.practicum.ewm.events.service.EventService;
-
+import ru.practicum.ewm.service.validation.Create;
+import ru.practicum.ewm.service.validation.Update;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
+@Validated
 @Slf4j
 public class EventPrivateController {
 
@@ -21,9 +26,9 @@ public class EventPrivateController {
 
     @GetMapping("/users/{userId}/events")
     public List<EventShortResponseDto> getEvents(
-            @PathVariable long userId,
-            @RequestParam(defaultValue = "0") int from,
-            @RequestParam(defaultValue = "10") int size
+            @PathVariable @Positive long userId,
+            @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+            @RequestParam(defaultValue = "10") @PositiveOrZero int size
     ) {
         log.info("Запрошен список событий пользователя с id = {} с параметрами: from={}; size={}", userId, from, size);
         return eventService.getEvents(userId, from, size);
@@ -31,8 +36,8 @@ public class EventPrivateController {
 
     @GetMapping("/users/{userId}/events/{eventId}")
     public EventResponseDto getEvent(
-            @PathVariable long userId,
-            @PathVariable long eventId
+            @PathVariable @Positive long userId,
+            @PathVariable @Positive long eventId
     ) {
         log.info("Запрошена информация о событии с id = {} пользователем с id = {}", eventId, userId);
         return eventService.getEvent(userId, eventId);
@@ -41,8 +46,8 @@ public class EventPrivateController {
     @PostMapping("/users/{userId}/events")
     @ResponseStatus(HttpStatus.CREATED)
     public EventResponseDto createEvent(
-            @PathVariable long userId,
-            @RequestBody EventDto eventDto
+            @PathVariable @Positive long userId,
+            @RequestBody @Validated(value = Create.class) EventDto eventDto
     ) {
         log.info("Пользователь с id = {} запросил создание события {}", userId, eventDto);
         return eventService.createEvent(userId, eventDto);
@@ -51,9 +56,9 @@ public class EventPrivateController {
     @PatchMapping("/users/{userId}/events/{eventId}")
     @ResponseStatus(HttpStatus.OK)
     public EventResponseDto updateEvent(
-            @PathVariable long userId,
-            @PathVariable long eventId,
-            @RequestBody EventDto eventDto
+            @PathVariable @Positive long userId,
+            @PathVariable @Positive long eventId,
+            @RequestBody @Validated (value = Update.class) EventDto eventDto
     ) {
         log.info("Пользователь с id = {} запросил обновление события с id = {} на {}", userId, eventId, eventDto);
         return eventService.userUpdateEvent(userId, eventId, eventDto);
