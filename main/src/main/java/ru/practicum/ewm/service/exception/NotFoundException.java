223e247be75f0a4b0bb00615dff04210efa415b6
MaricTheirin <ghostmeee@yandex.ru>
2023-05-03 17:51:44 +0300

    chore: Убрать лишний код

--- main/src/main/java/ru/practicum/ewm/service/exception/NotFoundException.java
+++ main/src/main/java/ru/practicum/ewm/service/exception/NotFoundException.java
@@ -4,10 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public abstract class NotFoundException extends EwmException {
 
-    private final HttpStatus status = HttpStatus.NOT_FOUND;
-
     public NotFoundException(String message) {
-        super(message);
+        super(message, HttpStatus.NOT_FOUND);
     }
 
 }
