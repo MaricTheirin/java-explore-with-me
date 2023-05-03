600ac009f0c8c4c31450aa5a2981c4e3df742cae
MaricTheirin <ghostmeee@yandex.ru>
2023-05-03 16:13:56 +0300

    chore: Добавить конструкторы/геттеры/сеттеры

--- main/src/main/java/ru/practicum/ewm/participations/dto/ParticipationResponseDto.java
+++ main/src/main/java/ru/practicum/ewm/participations/dto/ParticipationResponseDto.java
@@ -1,13 +1,15 @@
 package ru.practicum.ewm.participations.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Builder;
-import lombok.Value;
+import lombok.*;
 import lombok.extern.jackson.Jacksonized;
 import ru.practicum.ewm.participations.model.ParticipationState;
 import java.time.LocalDateTime;
 
-@Value
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Jacksonized
 public class ParticipationResponseDto {
