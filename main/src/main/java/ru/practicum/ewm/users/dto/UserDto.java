e1e64a5fc7433e56581d1f8783a8abe87edbbfe0
MaricTheirin <ghostmeee@yandex.ru>
2023-05-07 13:04:57 +0300

    chore: Убрать разделение на Create/Update

--- main/src/main/java/ru/practicum/ewm/users/dto/UserDto.java
+++ main/src/main/java/ru/practicum/ewm/users/dto/UserDto.java
@@ -2,8 +2,6 @@ package ru.practicum.ewm.users.dto;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
-import ru.practicum.ewm.service.validation.Create;
-import ru.practicum.ewm.service.validation.Update;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
@@ -17,7 +15,6 @@ public class UserDto {
     @NotBlank
     @Email(
             regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$",
-            groups = {Create.class, Update.class},
             message = "Email задан некорректно"
     )
     @Length(min = 3, max = 127, message = "Длина должна быть в промежутке от 3 до 127 символов")
